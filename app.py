dd315901ca0e653d9ea123639b365e0b84337f11
Caique Matos <43851217+cmatos17@users.noreply.github.com>
2022-07-11 10:39:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+from jinja2 import escape
+
 
 # Web App Title
 st.markdown('''
